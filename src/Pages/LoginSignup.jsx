c049ebb66daf97947779187a3289014c5f53744e
Guilherme Drumond Rosa <gdrumondrosa@gmail.com>
2024-02-05 16:23:23 -0300

    página de login

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>
-
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Seu Nome"/>
+          <input type="email" placeholder="Endereço de Email"/>
+          <input type="password" placeholder="Senha"/>
+        </div>
+        <button>Continuar</button>
+        <p className="loginsignup-login">Já possui uma conta? <span>Login aqui</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id=""/>
+          <p>Ao continuar, eu aceito os termos de uso & política de privacidade.</p>
+        </div>
+      </div>
     </div>
   )
 }
